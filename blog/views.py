12b6c465a3b2c33d537002ddd9343646b8c52713
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-06-13 23:45:52 +0600

    render data

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
+
 # Create your views here.
 class HomeView(ListView):
     model = Post
     template_name = "blog/index.html"
+    context_object_name = "posts"
