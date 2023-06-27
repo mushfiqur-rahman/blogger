fdf0100e96290f196ebbc2b7506efe46b0f4311b
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-06-27 17:03:35 +0600

    last commit

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from core.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
 
     try:
         from django.core.management import execute_from_command_line
